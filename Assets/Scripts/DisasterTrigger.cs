ced2853158b76888700ae0ca1a61f8de020b1214
glossimute <lifepattern_@outlook.com>
2025-06-02 18:23:01 +0900

    发送环境模拟启动信息与获取环境模拟信息

---
+++ Assets/Scripts/DisasterTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class DisasterTrigger : MonoBehaviour
+{
+    [ContextMenu("Trigger Disaster")]
+    public void TriggerDisaster()
+    {
+        StartCoroutine(PostDisaster());
+    }
+
+    IEnumerator PostDisaster()
+    {
+        string url = "http://localhost:8080/start_disaster";
+        UnityWebRequest request = UnityWebRequest.PostWwwForm(url, "");
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+            Debug.Log("managed to start disaster");
+        else
+            Debug.LogError("Failed to start disaster: " + request.error);
+    }
+}
